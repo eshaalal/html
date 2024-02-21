d5c3143dc51793ce38da10f34d9128f6a1739653
eshaalal <eshalal888@gmail.com>
2024-02-21 20:43:35 +0530

    changes

--- javascript/first.js
+++ javascript/first.js
@@ -1,19 +1,22 @@
 //console.log("Hii esha");
 // create a javascript code to find which one is greater a, b,c
-function Greatest(a, b, c) {
-    if (a>=b && a>=c) {
-        return a;
-    } else if (b>=a && b>=c) {
-        return b;
-    } else {
-        return c;
-    }
-}
+// function Greatest(a, b, c) {
+//     if (a>=b && a>=c) {
+//         return a;
+//     } else if (b>=a && b>=c) {
+//         return b;  
+//     } else {
+//         return c;
+//     }
+// }
 
 
-const a = 10;
-const b = 5;
-const c = 8;
+// const a = 10; 
+// const b = 5;
+// const c = 8;
 
-const greatest = Greatest(a, b, c);
-console.log("The greatest number is:", greatest);
+// const greatest = Greatest(a, b, c);
+// console.log("The greatest number is:", greatest);
+var esha=null;
+var esj;
+console.table([esha,esj]);
